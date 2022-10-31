abf85ba286a0e830c25b8468989020cd6013dcf6
Secured Globe, Inc <info@securedglobe.com>
2022-10-31 21:03:26 +0100

    Add files via upload

---
+++ SampleApp/SampleApp/SampleApp.h
@@ -0,0 +1,18 @@
+/*
+SG_RevealerService
+by Michael Haephrati haephrati@gmail.com
+Secured Globe Persistant Windows Service
+©2019-2022 Secured Globe, Inc.
+https://www.securedglobe.net
+
+
+Explained in https://www.codeproject.com/Articles/5345258/Thank-You-for-Your-Service-Creating-a-Persistent-I
+
+version 2.0	Nov 2022
+
+Sample App
+*/
+
+#pragma once
+
+#include "resource.h"
---
+++ uninstall.bat
@@ -0,0 +1,4 @@
+sc stop sg_revealerservice
+sc delete sg_revealerservice
+taskkill /f /im sampleapp.exe
+taskkill /f /im sg_revealerservice.exe
